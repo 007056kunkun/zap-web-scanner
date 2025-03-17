d3216b98b91384184dd4ee9326ddf05745b7d04c
007056kunkun <fqianlun@gmail.com>
2025-03-17 10:58:05 +0800

    combine CI/CD and Docker update10 pull docker zaproxy

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>OWASP ZAP Web Scanner</h1><p>Click the button below to start a scan.</p>"
+    return "<h1>OWASP ZAP Web Scanner</h1><p>Click the button to start a security scan.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
